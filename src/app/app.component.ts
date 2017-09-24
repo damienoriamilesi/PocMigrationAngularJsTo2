6e8c49949b72252e96cb7f780539bddeff432a18
damien <damien.oria@gmail.com>
2017-09-24 23:28:00 +0200

    Refactor Generic menu with json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
-//import { Router } from '@angular/router';
 
 @Component({
   selector: 'scam1-root',
@@ -9,62 +8,58 @@ import {AppRoutingModule} from './app-routing.module';
 })
 export class AppComponent {
   title = 'My Angular App';
-  heroes = [{Name:'Spiderman'}];
-  // constructor(private router:Router){
-    
-  // }
-  
-  constructor(private routingModule:AppRoutingModule) {
-          
+  heroes = [{Name: 'Spiderman'}];
+  constructor(private routingModule: AppRoutingModule) {
   }
 
-  menuItems = 
-        [
-            {                   
-                "label":"Menu1", 
-                "menuItems":
-                [
-                    { "label":"Menu-1-1", "routeLink":"component1"},
-                    { "label":"Menu-1-2"},
-                    { "label":"Menu-1-3" },
-                    { "label":"Menu-1-4" },
-                    { "label":"Menu-1-5" },
-                    { "label":"Menu-1-6" },
-                    { "label":"Menu-1-7" },
-                    { "label":"Menu-1-8" },
-                    { "label":"Menu-1-9" },
-                    { "label":"Menu-1-10" },
-                ]        
-            },
-            {                   
-                "label":"Menu 2", 
-                "menuItems":
-                [
-                    { "label":"Menu-2-1", "routeLink":"component2"},
-                    { "label":"Menu-2-2"},
-                    { "label":"Menu-2-3" },
-                    { "label":"Menu-2-4" },
-                    { "label":"Menu-2-5" },
-                    { "label":"Menu-2-6" },
-                    { "label":"Menu-2-7" },
-                    { "label":"Menu-2-8" },
-                    { "label":"Menu-2-9" },
-                    { "label":"Menu-2-10" , 
-                        "menuItems":
-                        [
-                            { "label":"Menu-2-1", "routeLink":"component2"},
-                            { "label":"Menu-2-2"},
-                            { "label":"Menu-2-3" },
-                            { "label":"Menu-2-4" },
-                            { "label":"Menu-2-5" },
-                            { "label":"Menu-2-6" },
-                            { "label":"Menu-2-7" },
-                            { "label":"Menu-2-8" },
-                            { "label":"Menu-2-9" },
-                            { "label":"Menu-2-10" },
-                        ]  
-                    },
-                ]        
-            }
-        ];
+  @Input() menuItems = menu;
 }
+
+const menu =
+[
+    {
+        'label': 'Menu-1',
+        'menuItems':
+        [
+            { 'label': 'Menu-1-1', 'routeLink': 'component1'},
+            { 'label': 'Menu-1-2'},
+            { 'label': 'Menu-1-3' },
+            { 'label': 'Menu-1-4' },
+            { 'label': 'Menu-1-5' },
+            { 'label': 'Menu-1-6' },
+            { 'label': 'Menu-1-7' },
+            { 'label': 'Menu-1-8' },
+            { 'label': 'Menu-1-9' },
+            { 'label': 'Menu-1-10', 'menuItems':
+            [
+                { 'label': 'Menu-1-1', 'routeLink': 'component1'},
+                { 'label': 'Menu-1-2'},
+                { 'label': 'Menu-1-3' },
+                { 'label': 'Menu-1-4' },
+                { 'label': 'Menu-1-5' },
+                { 'label': 'Menu-1-6' },
+                { 'label': 'Menu-1-7' },
+                { 'label': 'Menu-1-8' },
+                { 'label': 'Menu-1-9' },
+                { 'label': 'Menu-1-10',  },
+            ]},
+        ]
+    },
+    {
+      'label': 'Menu-2',
+      'menuItems':
+      [
+          { 'label': 'Menu-2-1', 'routeLink': 'component1'},
+          { 'label': 'Menu-2-2'},
+          { 'label': 'Menu-2-3' },
+          { 'label': 'Menu-2-4' },
+          { 'label': 'Menu-2-5' },
+          { 'label': 'Menu-2-6' },
+          { 'label': 'Menu-2-7' },
+          { 'label': 'Menu-2-8' },
+          { 'label': 'Menu-2-9' },
+          { 'label': 'Menu-2-10' },
+      ]
+  },
+
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Component1Component } from './feature1/component1/component1.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Component1Component
+    Component1Component,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
