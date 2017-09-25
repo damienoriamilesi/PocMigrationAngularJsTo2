7438f9a999872ed2d1b90b1d4decd5d3f54e60bd
damien <damien.oria@gmail.com>
2017-09-25 16:24:17 +0200

    Add Class check

--- src/app/app-menu/menu.component.ts
+++ src/app/app-menu/menu.component.ts
@@ -14,4 +14,58 @@ export class MenuComponent implements OnInit {
   ngOnInit() {
   }
 
+  getSubmenuClass(menuItems: any): string {
+    if (menuItems) { return 'dropdown-submenu'; }
+    return '';
+  }
 }
+
+const menu =
+[
+    {
+        'label': 'Menu-1',
+        'menuItems':
+        [
+            { 'label': 'Menu-1-1', 'routeLink': 'component1'},
+            { 'label': 'Menu-1-2'},
+            { 'label': 'Menu-1-3' },
+            { 'label': 'Menu-1-4' },
+            { 'label': 'Menu-1-5' },
+            { 'label': 'Menu-1-6' },
+            { 'label': 'Menu-1-7' },
+            { 'label': 'Menu-1-8' },
+            { 'label': 'Menu-1-9' },
+            { 'label': 'Menu-1-10', 'menuItems':
+            [
+                { 'label': 'Menu-1-1', 'routeLink': 'component1'},
+                { 'label': 'Menu-1-2'},
+                { 'label': 'Menu-1-3' },
+                { 'label': 'Menu-1-4' },
+                { 'label': 'Menu-1-5' },
+                { 'label': 'Menu-1-6' },
+                { 'label': 'Menu-1-7' },
+                { 'label': 'Menu-1-8' },
+                { 'label': 'Menu-1-9' },
+                { 'label': 'Menu-1-10',  },
+            ]},
+        ]
+    },
+    {
+      'label': 'Menu-2',
+      'menuItems':
+      [
+          { 'label': 'Menu-2-1', 'routeLink': 'component1'},
+          { 'label': 'Menu-2-2'},
+          { 'label': 'Menu-2-3' },
+          { 'label': 'Menu-2-4' },
+          { 'label': 'Menu-2-5' },
+          { 'label': 'Menu-2-6' },
+          { 'label': 'Menu-2-7' },
+          { 'label': 'Menu-2-8' },
+          { 'label': 'Menu-2-9' },
+          { 'label': 'Menu-2-10' },
+      ]
+  },
+
+];
+
